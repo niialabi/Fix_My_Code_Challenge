6f0549fb241420937c8a71edf3b4613fb655cad8
Taufiq Mohammed Alabi <taufiq.alabi@gmail.com>
2023-12-17 07:33:32 -0800

    square class fix

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module containing Square class """
+
+
+class Square():
+    """ square class """
+
+    def __init__(self, *args, **kwargs):
+        width = kwargs.get("width", 0)
+        height = kwargs.get("height", 0)
+
+        if width != height:
+            raise ValueError("Width and height must be equal.")
+
+        self.width = width
+        self.height = height
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PerimiterOfMySquare(self):
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=12)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PerimiterOfMySquare())
